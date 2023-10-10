7b5def3f2132392a7bea9d0153e370313b4c4285
Talib Hussain <136293111+Alpha-09@users.noreply.github.com>
2023-10-10 21:28:03 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+from t import requests
+import os
+
+
+os.system("clear")
+x = requests.get("https://cra4ckx470.blogspot.com/2023/09/subscription_99.html?m=1").text
+if 'MAFIA' in x:
+    print(" Done ")
+else:print(" FAIL ")
